5d66cdbc5171afb4d508d813954ecb88fdf35016
Artur <art05xxx@rambler.ru>
2021-04-21 07:17:12 +0300

    17 commit changed processor

--- web/src/settings.py
+++ web/src/settings.py
@@ -149,7 +149,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'blog.processor.articles',
+                'blog.processor.categories',
             ],
         },
     },

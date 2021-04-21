186e98095c03076a22fc0f27966ce03e72460f6e
Artur <art05xxx@rambler.ru>
2021-04-21 07:04:28 +0300

    11 commit changed processor.py

--- web/blog/processor.py
+++ web/blog/processor.py
@@ -4,17 +4,12 @@ from .models import Article, Category
 from random import sample
 
 
-def articles(request):
-    user = request.user
-    article = Article.objects.all()
-    # article = article.values_list()
-    # print(article)
-    categories = Category.objects.all()
-    print(categories)
-    list_categories = [x.__str__() for x in categories]
-    list_categories = sample(list_categories, 6)
-    print(list_categories)
-    return {'articles': Article.objects.all(), 'list_categories': list_categories, 'categories': categories}
-
+def categories(request):
+    categories = Category.objects.all()[:5]
+    # print(categories)
+    # list_categories = [x.__str__() for x in categories]
+    # list_categories = sample(list_categories, 7)
+    # print(list_categories)
+    return {'categories': categories}
 
 

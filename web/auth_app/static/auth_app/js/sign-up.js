3d32f8b5c02323db61b947f667498392d9996cab
Artur <art05xxx@rambler.ru>
2021-04-30 07:24:24 +0300

    22 commit craeted function in sign-up.js

--- web/auth_app/static/auth_app/js/sign-up.js
+++ web/auth_app/static/auth_app/js/sign-up.js
@@ -1,1 +1,37 @@
 console.log('sing-up')
+
+$(function(){
+    $('#signUpForm').submit(signUp);
+})
+
+function signUp(event){
+    event.preventDefault();
+    console.log('click')
+    let form=$(this);
+    console.log(form)
+    let formData=form.serialize()
+    console.log(formData)
+    let path=form.attr('action')
+    let method=form.attr('method')
+    console.log(path, method)
+    $.ajax({
+        url:path,
+        type:method,
+        data:formData,
+        success:function(data){
+            console.log(data, 'success')
+        },
+        error:function(data){
+            console.log(data, 'error')
+            if (data.responseJSON.email){
+                $('#emailGroup').addClass('has-error')
+                $('#emailGroup').append('<div class="help-block">' + data.responseJSON.email + '</div>')}
+            if (data.responseJSON.password1){
+                $('#password1Group').addClass('has-error')
+                $('#password1Group').append('<div class="help-block">' + data.responseJSON.password1 + '</div>')}
+            if (data.responseJSON.password2){
+                $('#password2Group').addClass('has-error')
+                $('#password2Group').append('<div class="help-block">' + data.responseJSON.password2 + '</div>')}
+        }
+    })
+}

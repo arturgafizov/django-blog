8fa799770e33bb46b3800127f9a4758264d8afbe
Artur <art05xxx@rambler.ru>
2021-04-30 07:24:00 +0300

    21 commit craeted function in detail.js

--- web/blog/static/blog/js/detail.js
+++ web/blog/static/blog/js/detail.js
@@ -1,6 +1,37 @@
 $(function () {
-
+    $('#formReview').submit(leaveComment)
 
 });
 
 console.log('blog-detail')
+
+function leaveComment(event){
+    console.log('leave_comment')
+    event.preventDefault()
+    let form=$(this)
+    console.log(form)
+    data = form.serialize()
+    console.log(data)
+    let url=form.attr('action')
+    let method=form.attr('method')
+    console.log(url, method)
+//    $.ajax({
+//        url: url,
+//        type: method,
+//        data: data,
+//        success: function (data){
+//            console.log('success', data)
+//            location.reload()
+//        },
+//        error: function (data){
+//            console.log('error', data)
+//        },
+//    })
+    $.post('/action', {query: 'test'}, function(data) {
+    console.log(data); // ответ от сервера
+    })
+    .success(function() { console.log('Успешное выполнение'); })
+    .error(function(jqXHR) { console.log('Ошибка выполнения'); })
+    .complete(function() { console.log('Завершение выполнения'); });
+
+}

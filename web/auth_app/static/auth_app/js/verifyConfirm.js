d8e18b90f5e3160891fb281e008d569baa292215
Artur <art05xxx@rambler.ru>
2021-05-10 01:36:25 +0300

    44 add function verifyEmail

--- web/auth_app/static/auth_app/js/verifyConfirm.js
+++ web/auth_app/static/auth_app/js/verifyConfirm.js
@@ -1,1 +1,25 @@
 console.log('verifyConfirm')
+
+$(function(){
+  verifyEmail()
+})
+
+function verifyEmail() {
+  let url = window.location.pathname
+  console.log(url.split('/'))
+  console.log(key = url.split('/')[3])
+  $.ajax({
+      url: '/auth/sign-up/verify/',
+      type: 'post',
+      data: {'key': url.split('/')[3]},
+      success:function(data){
+          console.log(data, 'success')
+          let url = '/auth/login/'
+          window.location.href=url
+      },
+      error: function (data) {
+          console.log(data);
+          }
+     })
+
+}

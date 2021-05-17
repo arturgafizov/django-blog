066be9b9f2f344d75c35c2c14d5d22c3f22cdc39
Artur <art05xxx@rambler.ru>
2021-05-17 21:52:23 +0300

    53 add field file and update model? serializer, view

--- web/contact_us/models.py
+++ web/contact_us/models.py
@@ -8,6 +8,7 @@ class Feedback(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     content = models.TextField()
+    file = models.FileField(null=True)
 
     class Meta:
         verbose_name = _('Feedback')
--- web/contact_us/serializers.py
+++ web/contact_us/serializers.py
@@ -3,12 +3,12 @@ from .models import Feedback
 
 
 class FeedbackSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=False)
-    name = serializers.CharField(min_length=2, required=False)
+    email = serializers.EmailField(required=True)
+    name = serializers.CharField(min_length=2, required=True)
 
     class Meta:
         model = Feedback
-        fields = ('name', 'email', 'content')
+        fields = ('name', 'email', 'content', 'file')
 
     def create(self, validated_data):
         user = self.context['request'].user

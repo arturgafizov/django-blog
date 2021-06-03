b5a9afa91fcb0f929d1c7c0996c7167fffe78376
Artur <art05xxx@rambler.ru>
2021-06-03 07:16:39 +0300

    73 cleaned file

--- web/main/services.py
+++ web/main/services.py
@@ -4,8 +4,7 @@ from rest_framework.reverse import reverse_lazy
 from auth_app.utils import get_activate_key
 from main.decorators import except_shell
 from src.celery import app
-from auth_app.tasks import test, sent_verify_email, sent_password_reset
-from main.tasks import sent_email_user, send_information_email
+from main.tasks import send_information_email
 from django.conf import settings
 
 User = get_user_model()
@@ -67,18 +66,6 @@ class CeleryService:
         }
         to_email = user.email,
         send_information_email.delay(subject, html_email_template_name, context, to_email)
-
-    # @staticmethod
-    # def send_email_confirm(user):
-    #     key = get_activate_key(user)
-    #     kwargs = {
-    #         'to_email': user.email,
-    #         'content': {
-    #             'user': user.get_full_name(),
-    #             'activate_url': key,
-    #         }
-    #     }
-    #     sent_verify_email.delay(**kwargs)
 
 
 class UserService:

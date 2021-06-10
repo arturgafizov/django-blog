6e27113cf431e9993c31ecc4149d9cf9aac18cd5
Artur <art05xxx@rambler.ru>
2021-06-10 22:48:01 +0300

    84 add change_profile.js

---
+++ web/profiles/static/profile/change_profile.js
@@ -0,0 +1,43 @@
+console.log('changeProfile')
+
+$(function () {
+  $('#changeProfileForm').submit(profile);
+});
+
+function profile(event) {
+    event.preventDefault();
+    console.log('click')
+    let form = $(this);
+    let formData = {
+        "user": {
+          'first_name': $("#first_nameGroup").val(),
+          'last_name': $('#last_nameGroup').val(),
+          'email': $('#emailGroup').val(),
+        },
+        'mobile': $('#mobileGroup').val(),
+        'location': $('#locationGroup').val(),
+    }
+    console.log(formData)
+    let method=form.attr('method')
+    let path=form.attr('action')
+    console.log(path, method)
+    $.ajax({
+        url: path,
+        type: method,
+        data: form.formData,
+        success: function (data) {
+            console.log(data, 'success')
+
+        },
+        error: function (data) {
+            $("#first_nameGroup").addClass("has-error");
+            $("#last_nameGroup").addClass("has-error");
+            $("#mobileGroup").addClass("has-error");
+            $("#emailGroup").addClass("has-error");
+            $("#emailGroup").append(
+              '<div class="help-block">' + data.responseJSON.email + "</div>"
+            );
+            $("#locationGroup").addClass("has-error");
+        }
+      })
+}

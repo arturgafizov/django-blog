d1cc1bd01b30b3483b90eca558a71f734e58f579
Artur <art05xxx@rambler.ru>
2021-06-10 22:52:36 +0300

    90 add url new-post

--- web/blog/urls.py
+++ web/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from rest_framework.routers import DefaultRouter
-
+from main.views import TemplateAPIView
 from . import views
 
 app_name = 'blog'
@@ -12,7 +12,7 @@ router.register('posts', views.ArticleViewSet, basename='post')
 router.register('comment', views.CommentViewSet, basename='comment')
 
 urlpatterns = [
-
+    path('new-article/', views.NewArticleView.as_view(template_name='blog/post_create.html'), name='new-post')
 ]
 
 urlpatterns += router.urls

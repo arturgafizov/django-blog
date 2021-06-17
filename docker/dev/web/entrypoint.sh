fef666e99df570d4a02d193c4cd5e99b957c1845
Artur <art05xxx@rambler.ru>
2021-06-17 07:08:29 +0300

    91 correct in templates

--- docker/dev/web/entrypoint.sh
+++ docker/dev/web/entrypoint.sh
@@ -4,8 +4,8 @@ python manage.py wait_for_db
 
 python manage.py makemigrations
 python manage.py migrate
-#python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
 
 # If you use Uvicorn please also run collectstatic command
-python manage.py collectstatic --no-input
-uvicorn src.asgi:application --host 0.0.0.0 --port 8000 --log-level debug --reload
+#python manage.py collectstatic --no-input
+#uvicorn src.asgi:application --host 0.0.0.0 --port 8000 --log-level debug --reload

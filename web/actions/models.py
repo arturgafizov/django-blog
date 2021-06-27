0890112821d00477f5692373b92a2c5edd769246
Artur <art05xxx@rambler.ru>
2021-06-27 12:25:00 +0300

    126 created  choices and models in app Actions

---
+++ web/actions/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .choices import LikeStatus
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+# Create your models here.
+User = get_user_model()
+
+
+class LikeDislike(models.Model):
+    vote = models.IntegerField(choices=LikeStatus.choices)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    date = models.DateTimeField(auto_now=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()

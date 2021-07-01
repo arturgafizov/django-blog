ce46f6203ad98a504cf9cbb9a44625721c218a42
Artur <art05xxx@rambler.ru>
2021-07-01 07:20:01 +0300

    135 added path change_password, and LikeDislikeView

--- web/actions/views.py
+++ web/actions/views.py
@@ -1,13 +1,26 @@
 import logging
 from django.utils.translation import gettext_lazy as _
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 
 from . import services
-from . import serializers
+from .models import LikeDislike
+from .serializers import LikeDislikeSerializer
 
 logger = logging.getLogger(__name__)
 
-# Create your views here.
+
+class LikeDislikeView(GenericAPIView):
+    serializer_class = LikeDislikeSerializer
+
+    def get_queryset(self):
+        return LikeDislike.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({}, status=status.HTTP_201_CREATED)
--- web/profiles/urls.py
+++ web/profiles/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from . views import ProfileRetrieveView, UploadAvatarView
+from . views import ProfileRetrieveView, UploadAvatarView, ProfileViewSet
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('', ProfileRetrieveView.as_view(), name='profile_detail'),
     path('avatar/', UploadAvatarView.as_view(), name='avatar_upload'),
-
+    path('profile/change-password/', ProfileViewSet.as_view({'post': 'change_password'}), name='change_password')
 ]

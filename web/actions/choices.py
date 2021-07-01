8c8243131ae9c6e0e146b13d7b15eb8e95378e50
Artur <art05xxx@rambler.ru>
2021-07-01 07:02:17 +0300

    130 add LikeObjChoices

--- web/actions/choices.py
+++ web/actions/choices.py
@@ -1,6 +1,11 @@
-from django.db.models import IntegerChoices
+from django.db.models import IntegerChoices, TextChoices
 
 
 class LikeStatus(IntegerChoices):
     LIKE = (1, 'like')
     DISLIKE = (-1, 'dislike')
+
+
+class LikeObjChoices(TextChoices):
+    ARTICLE = ('article', 'Article')
+    COMMENT = ('comment', 'Comment')

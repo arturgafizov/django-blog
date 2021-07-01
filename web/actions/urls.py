1e0138a223dff54b308de0d9954508b04db8b842
Artur <art05xxx@rambler.ru>
2021-07-01 07:20:50 +0300

    136 added path like_dislike

--- web/actions/urls.py
+++ web/actions/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from . import views
+from .views import LikeDislikeView
 
 app_name = 'actions'
 
 router = DefaultRouter()
+# router.register('actions', LikeDislikeViewSet, basename='like_dislike')
+
 
 urlpatterns = [
-
+    path('actions/', LikeDislikeView.as_view(), name='like_dislike')
 ]
 
 urlpatterns += router.urls

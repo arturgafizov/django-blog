d7477d0be7df22112e8e4393b945e3f3dba7e3d2
Artur <art05xxx@rambler.ru>
2021-07-01 07:16:00 +0300

    134 added LikeDislikeSerializer

--- web/actions/serializers.py
+++ web/actions/serializers.py
@@ -1,3 +1,17 @@
 from rest_framework import serializers
 
-# Create your serializers here.
+from django.contrib.auth import get_user_model
+from .models import LikeDislike
+from main.serializers import UserSerializer
+from .choices import LikeObjChoices, LikeStatus
+User = get_user_model()
+
+
+class LikeDislikeSerializer(serializers.Serializer):
+    model = serializers.ChoiceField(choices=LikeObjChoices.choices)
+    vote = serializers.ChoiceField(choices=LikeStatus.choices)
+    object_id = serializers.IntegerField()
+
+    def save(self):
+        user = self.context['request'].user
+        print(self.validated_data)

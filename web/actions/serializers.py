e24b4ef289c46d0951361b1b8e48b162a1c74c94
Artur <art05xxx@rambler.ru>
2021-07-04 18:51:03 +0300

    146 update method save

--- web/actions/serializers.py
+++ web/actions/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
 from django.contrib.auth import get_user_model
-from .models import LikeDislike
 from main.serializers import UserSerializer
 from .choices import LikeObjChoices, LikeStatus
+from blog.services import BlogService
+from .services import ActionsService
+from blog.models import Article, Comment
+
 User = get_user_model()
 
 
@@ -13,5 +16,18 @@ class LikeDislikeSerializer(serializers.Serializer):
     object_id = serializers.IntegerField()
 
     def save(self):
-        # user = self.context['request'].user
+        user = self.context['request'].user
         print(self.validated_data)
+        model = self.validated_data['model']
+        object_id = self.validated_data['object_id']
+        vote = self.validated_data['vote']
+        if model == LikeObjChoices.ARTICLE:
+            obj: Article = BlogService.get_article(object_id)
+        else:
+            obj: Comment = BlogService.get_comment(object_id)
+        like_dislike = ActionsService.get_like_dislike_obj(user, obj, object_id)
+        print(obj, like_dislike)
+        if not like_dislike:
+            obj.votes.create(user=user, vote=vote)
+        else:
+            pass

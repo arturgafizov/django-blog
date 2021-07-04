40be8f87dd476e335eb5254af99cb793a455a4f1
Artur <art05xxx@rambler.ru>
2021-07-04 18:47:44 +0300

    142 created ActionsService

--- web/actions/services.py
+++ web/actions/services.py
@@ -1,7 +1,13 @@
 from django.conf import settings
 
-from . import models
+from .models import LikeDislike
+from django.contrib.contenttypes.models import ContentType
+from main.decorators import except_shell
 
 
 class ActionsService:
-    pass
+    @staticmethod
+    @except_shell((LikeDislike.DoesNotExist, ))
+    def get_like_dislike_obj(user, model, object_id):
+        content_type = ContentType.objects.get_for_model(model)
+        return LikeDislike.objects.get(user=user, content_type=content_type, object_id=object_id)

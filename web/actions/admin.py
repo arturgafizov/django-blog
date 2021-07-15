056dca3980d8961877eaeafe4b8163389d0af32b
Artur <art05xxx@rambler.ru>
2021-07-15 07:24:54 +0300

    163 add register Follower model in admin.py

--- web/actions/admin.py
+++ web/actions/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from actions.models import LikeDislike
+from actions.models import LikeDislike, Follower
 
 
 @admin.register(LikeDislike)
 class LikeDislikeAdmin(admin.ModelAdmin):
     list_display = ('vote', 'user', 'date')
+
+
+@admin.register(Follower)
+class FollowerAdmin(admin.ModelAdmin):
+    list_display = ('subscriber', 'to_user', 'date')

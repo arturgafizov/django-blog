cb772cc933d5cc397f0d3871d34676aedae16aba
Artur <art05xxx@rambler.ru>
2021-07-18 20:01:31 +0300

    177 added router profile

--- web/profiles/urls.py
+++ web/profiles/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
-from . views import ProfileRetrieveView, UploadAvatarView, ProfileViewSet
+from . views import ProfileRetrieveView, UploadAvatarView, ProfileViewSet, ProfileDetailViewSet
 
 app_name = 'profiles'
+
+router = DefaultRouter()
+router.register('profile', ProfileDetailViewSet , basename='profile')
+
 
 urlpatterns = [
     path('', ProfileRetrieveView.as_view(), name='profile_detail'),
     path('avatar/', UploadAvatarView.as_view(), name='avatar_upload'),
     path('profile/change-password/', ProfileViewSet.as_view({'post': 'change_password'}), name='change_password')
 ]
+
+urlpatterns += router.urls

80c5ccf70670ccdf5acb35affb86f7f18acadc30
Artur <art05xxx@rambler.ru>
2021-07-18 19:59:44 +0300

    175 added field avatar

--- web/main/serializers.py
+++ web/main/serializers.py
@@ -5,6 +5,8 @@ User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
+    avatar = serializers.ImageField(source='profiles_set.avatar')
+
     class Meta:
         model = User
-        fields = ('id', 'full_name', 'email', 'first_name', 'last_name',)
+        fields = ('id', 'full_name', 'email', 'first_name', 'last_name', 'avatar',)

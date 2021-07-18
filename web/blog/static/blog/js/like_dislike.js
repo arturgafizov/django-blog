f288a8056df1aa8a820ce6835254e2c566f1c399
Artur <art05xxx@rambler.ru>
2021-07-18 19:56:13 +0300

    171 update functions

--- web/blog/static/blog/js/like_dislike.js
+++ web/blog/static/blog/js/like_dislike.js
@@ -4,11 +4,9 @@ console.log('like_dislike')
 $(function () {
   $('#articleLike').click(like);
   $('#articleDislike').click(like);
-  $('.commentLike').click(like);
-  $('.commentDislike').click(like);
+  $('.commentLike').click(comDislike);
+  $('.commentDislike').click(comDislike);
 });
-
-
 
 function like(event) {
     event.preventDefault();
@@ -29,9 +27,50 @@ function like(event) {
             console.log(data, 'success')
             $('#likeCount').text(data.like_count)
             $('#dislikeCount').text(data.dislike_count)
-            $('#likeCountComment').text(data.like_count)
-            $('#dislikeCountComment').text(data.dislike_count)
         },
+        error: function (data) {
+            console.log('error', data)
+            error_process(data);
+        }
+      })
+}
+
+
+function comDislike(event) {
+    event.preventDefault();
+    console.log('com_click')
+    let like = $(this);
+    let vote = like.data('vote')
+    let data = {
+      'object_id': like.data('id'),
+      'model': like.data('model'),
+      'vote': vote,
+    }
+    console.log(data)
+    let data_id = this.getAttribute('data-id');
+    console.log(data_id)
+
+
+    $.ajax({
+        url: like.data('href'),
+        type: "POST",
+        data: data,
+        success: function (data) {
+            console.log(data, 'success')
+            $('#likeCount_'+data_id).text(data.like_count)
+            $('#dislikeCount_'+data_id).text(data.dislike_count)
+            if (vote === 1 ){
+              console.log('like', vote, data_id, like)
+              $('#comLike_'+data_id).attr('style', 'background:blue');
+              $('#comDislike_'+data_id).attr('style', 'background:#03A9F4')
+            }
+            else {
+              console.log('dislike', vote, data_id, like)
+              $('#comDislike_'+data_id).attr('style', 'background:#03A9F4')
+              $('#comLike_'+data_id).attr('style', 'background:blue')
+            }
+        },
+
         error: function (data) {
             console.log('error', data)
             error_process(data);
@@ -82,3 +121,25 @@ document.addEventListener("click", function(thenews)
     }
   });
 
+
+// var color1 = $('.commentLike')[0];
+// var color2 = document.getElementsByClassName('commentDislike')[0];
+//
+// function commentLike() {
+//   if (color1.style.background == "blue") {
+//     color1.style.background = "#03A9F4";
+//   } else {
+//     color1.style.background = "blue";
+//   }
+//   color2.style.background = "#03A9F4";
+//   }
+//
+// function commentDislike() {
+//     if (color2.style.background == "blue" ) {
+//       color2.style.background = "#03A9F4";
+//       }
+//     else {
+//       color2.style.background = "blue";
+//       }
+//     color1.style.background = "#03A9F4";
+// }

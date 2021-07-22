303d9a89adc6dff1e21bddcd6c09ccb0afce1163
Artur <art05xxx@rambler.ru>
2021-07-22 07:08:19 +0300

    186 added def get_follower

--- web/actions/services.py
+++ web/actions/services.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 
-from .models import LikeDislike
+from .models import LikeDislike, Follower
 from django.contrib.contenttypes.models import ContentType
 from main.decorators import except_shell
 
@@ -11,3 +11,8 @@ class ActionsService:
     def get_like_dislike_obj(user, model, object_id):
         content_type = ContentType.objects.get_for_model(model)
         return LikeDislike.objects.get(user=user, content_type=content_type, object_id=object_id)
+
+    @staticmethod
+    @except_shell((Follower.DoesNotExist, ))
+    def get_follower(user_id: int):
+        return Follower.objects.filter(subscriber_id=user_id)

63d0b4628ca89f545b6a772caad971e35d560a72
Artur <art05xxx@rambler.ru>
2021-07-22 07:04:58 +0300

    182 update like function

--- web/blog/static/blog/js/like_dislike.js
+++ web/blog/static/blog/js/like_dislike.js
@@ -2,8 +2,8 @@ console.log('like_dislike')
 
 
 $(function () {
-  $('#articleLike').click(like);
-  $('#articleDislike').click(like);
+  $('.articleLike').click(like);
+  $('.articleDislike').click(like);
   $('.commentLike').click(comDislike);
   $('.commentDislike').click(comDislike);
 });
@@ -11,13 +11,18 @@ $(function () {
 function like(event) {
     event.preventDefault();
     console.log('click')
-
+    let test = ''
+    let url = window.location.pathname
     let like = $(this);
+    let vote = like.data('vote')
     let data = {
       'object_id': like.data('id'),
       'model': like.data('model'),
       'vote': like.attr('data-vote'),
     }
+    let data_id = this.getAttribute('data-id');
+    console.log(data_id)
+
     console.log(data)
     $.ajax({
         url: like.data('href'),
@@ -27,10 +32,27 @@ function like(event) {
             console.log(data, 'success')
             $('#likeCount').text(data.like_count)
             $('#dislikeCount').text(data.dislike_count)
+            let count_dislike = (data.dislike_count)
+            let count_like = (data.like_count)
+
+            if (vote === 1 && data.like_status === 'liked'){
+              console.log('like', vote, data_id, count_like)
+              $('#articleLike_'+data_id).attr('style', 'background:blue');
+              $('#articleDislike_'+data_id).attr('style', 'background:#03A9F4')
+            }
+            else if (vote === -1 && data.like_status === 'disliked'){
+              $('#articleDislike_'+data_id).attr('style', 'background:blue');
+              $('#articleLike_'+data_id).attr('style', 'background:#03A9F4')
+            }
+            else {
+              console.log('dislike', vote, data_id, count_dislike)
+              $('#articleDislike_'+data_id).attr('style', 'background:#03A9F4')
+              $('#articleLike_'+data_id).attr('style', 'background:#03A9F4')
+            }
         },
         error: function (data) {
             console.log('error', data)
-            error_process(data);
+
         }
       })
 }
@@ -50,7 +72,6 @@ function comDislike(event) {
     let data_id = this.getAttribute('data-id');
     console.log(data_id)
 
-
     $.ajax({
         url: like.data('href'),
         type: "POST",
@@ -59,15 +80,22 @@ function comDislike(event) {
             console.log(data, 'success')
             $('#likeCount_'+data_id).text(data.like_count)
             $('#dislikeCount_'+data_id).text(data.dislike_count)
-            if (vote === 1 ){
-              console.log('like', vote, data_id, like)
+            let count_dislike = (data.dislike_count)
+            let count_like = (data.like_count)
+
+            if (vote === 1 && count_like === 1){
+              console.log('like', vote, data_id, count_like)
               $('#comLike_'+data_id).attr('style', 'background:blue');
               $('#comDislike_'+data_id).attr('style', 'background:#03A9F4')
             }
+            else if (vote === -1 && count_dislike === 1){
+              $('#comDislike_'+data_id).attr('style', 'background:blue')
+              $('#comLike_'+data_id).attr('style', 'background:#03A9F4')
+            }
             else {
-              console.log('dislike', vote, data_id, like)
+              console.log('dislike', vote, data_id, count_dislike)
               $('#comDislike_'+data_id).attr('style', 'background:#03A9F4')
-              $('#comLike_'+data_id).attr('style', 'background:blue')
+              $('#comLike_'+data_id).attr('style', 'background:#03A9F4')
             }
         },
 
@@ -78,68 +106,48 @@ function comDislike(event) {
       })
 }
 
-
-document.addEventListener("click", function(thenew)
-  {
-    articleLike = thenew . target. id;
-    the_class = thenew . target.className;
-    if(the_class == "social-like")
-    {
-      articleLike = document.getElementById(articleLike);
-      if(articleLike .style . background == "blue")
-      {
-        articleLike .style . background = "#03A9F4";
-      }
-      else
-      { var links = document.querySelectorAll(".social-like");
-        links.forEach(link => {
-          link.setAttribute("style", "background:#03A9F4");
-        })
-        articleLike .style . background = "blue";
-      }
-    }
-  });
-
-document.addEventListener("click", function(thenews)
-  {
-    articleDislike = thenews . target. id;
-    the_class = thenews . target.className;
-    if(the_class == "social-dislike")
-    {
-      articleDislike = document.getElementById(articleDislike);
-      if(articleDislike .style . background == "blue")
-      {
-        articleDislike .style . background = "#03A9F4";
-      }
-      else
-      { var links = document.querySelectorAll(".social-dislike");
-        links.forEach(link => {
-          link.setAttribute("style", "background:#03A9F4");
-        })
-        articleDislike .style . background = "blue";
-      }
-    }
-  });
-
-
-// var color1 = $('.commentLike')[0];
-// var color2 = document.getElementsByClassName('commentDislike')[0];
 //
-// function commentLike() {
-//   if (color1.style.background == "blue") {
-//     color1.style.background = "#03A9F4";
-//   } else {
-//     color1.style.background = "blue";
-//   }
-//   color2.style.background = "#03A9F4";
-//   }
+// document.addEventListener("click", function(thenew)
+//   {
+//     articleLike = thenew . target. id;
+//     the_class = thenew . target.className;
+//     if(the_class == "social-like")
+//     {
+//       articleLike = document.getElementById(articleLike);
+//       if(articleLike.style.background == "blue")
+//       {
+//         articleLike.style.background = "#03A9F4";
+//       }
+//       else
+//       { var links = document.querySelectorAll(".social-like");
+//         links.forEach(link => {
+//           link.setAttribute("style", "background:#03A9F4");
+//         })
+//         articleLike.style.background = "blue";
+//       }
+//     }
+//   });
 //
-// function commentDislike() {
-//     if (color2.style.background == "blue" ) {
-//       color2.style.background = "#03A9F4";
+// document.addEventListener("click", function(thenews)
+//   {
+//     articleDislike = thenews . target. id;
+//     the_class = thenews . target.className;
+//     if(the_class == "social-dislike")
+//     {
+//       articleDislike = document.getElementById(articleDislike);
+//       if(articleDislike.style.background == "blue")
+//       {
+//         articleDislike.style.background = "#03A9F4";
 //       }
-//     else {
-//       color2.style.background = "blue";
+//       else
+//       { var links = document.querySelectorAll(".social-dislike");
+//         links.forEach(link => {
+//           link.setAttribute("style", "background:#03A9F4");
+//         })
+//         articleDislike.style.background = "blue";
 //       }
-//     color1.style.background = "#03A9F4";
-// }
+//     }
+//   });
+
+
+

6f5f4ac05b1be6bcad8e57dbaf3f0c8d8ec3da0f
Artur <art05xxx@rambler.ru>
2021-07-23 07:28:06 +0300

    189  update model

--- web/actions/migrations/0002_follower.py
+++ web/actions/migrations/0002_follower.py
@@ -18,8 +18,10 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('date', models.DateTimeField(auto_now_add=True)),
-                ('subscriber', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='following', to=settings.AUTH_USER_MODEL)),
-                ('to_user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='followers', to=settings.AUTH_USER_MODEL)),
+                ('subscriber', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='following',
+                                                 to=settings.AUTH_USER_MODEL)),
+                ('to_user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='followers',
+                                              to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'ordering': ('-date',),

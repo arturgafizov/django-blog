428fa47c61c2914097b5fdbd303e2dd82d4a1e80
Artur <art05xxx@rambler.ru>
2021-07-24 18:23:31 +0300

    204 added get_follow_status in UserSerializer

--- web/main/serializers.py
+++ web/main/serializers.py
@@ -1,15 +1,25 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+from actions.choices import FollowStatus
+
 User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
     avatar = serializers.ImageField(source='profiles_set.avatar')
+    follow = serializers.SerializerMethodField(method_name='get_follow_status')
 
     class Meta:
         model = User
-        fields = ('id', 'full_name', 'email', 'first_name', 'last_name', 'avatar',)
+        fields = ('id', 'full_name', 'email', 'first_name', 'last_name', 'avatar', 'follow')
         extra_kwargs = {
             'avatar': {'read_only': True},
         }
+
+    def get_follow_status(self, obj):
+        user = self.context['request'].user
+        follow_obj = user.following.filter(to_user=obj).exists()
+        if follow_obj:
+            return FollowStatus.UNFOLLOW
+        return FollowStatus.FOLLOW

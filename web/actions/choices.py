a0ddc53dd8e4fb343a160dc8cefbd83929c5afc9
Artur <art05xxx@rambler.ru>
2021-08-05 07:00:47 +0300

    208 create UserActionChoices

--- web/actions/choices.py
+++ web/actions/choices.py
@@ -21,3 +21,12 @@ class LikeIconStatus(TextChoices):
 class FollowStatus(TextChoices):
     FOLLOW = ('Follow', 'Follow')
     UNFOLLOW = ('Unfollow', 'Unfollow')
+
+
+class UserActionChoices(IntegerChoices):
+    FOLLOW_TO = (1, 'User {subscriber} follow to {to_user}')
+    UNFOLLOW_TO = (2, 'User {subscriber} follow to {to_user}')
+    LIKE_ARTICLE = (3, 'User {user} liked {article}')
+    DISLIKE_ARTICLE = (4, 'User {user} disliked {article}')
+    LIKE_COMMENT = (5, 'User {user} liked {comment}')
+    DISLIKE_COMMENT = (6, 'User {user} disliked {comment}')

1ffcc805f0e9fcec2da608b37297677b909f9ea4
Artur <art05xxx@rambler.ru>
2021-08-12 01:58:16 +0300

    221 added two urls

--- web/actions/urls.py
+++ web/actions/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import LikeDislikeView, FollowerView, UserActionView
+from .views import LikeDislikeView, FollowerView, UserActionView, UserFollowView
 
 app_name = 'actions'
 
@@ -12,7 +12,9 @@ router = DefaultRouter()
 urlpatterns = [
     path('actions/', LikeDislikeView.as_view(), name='like_dislike'),
     path('follow/', FollowerView.as_view(), name='follow'),
-    path('user-action/', UserActionView.as_view(), name='user_action')
+    path('user-action/', UserActionView.as_view(), name='user_action'),
+    path('user/followers/', UserFollowView.as_view({'get': 'get_followers'}), name='user-followers'),
+    path('user/following/', UserFollowView.as_view({'get': 'get_following'}), name='user-following'),
 ]
 
 urlpatterns += router.urls

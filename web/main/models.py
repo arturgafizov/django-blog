8d78b464afef596834299fb01c442c0c2e436124
Artur <art05xxx@rambler.ru>
2021-08-22 13:46:02 +0300

    234 added three function get_absolute_url, followers_count, following_count

--- web/main/models.py
+++ web/main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-
+from rest_framework.reverse import reverse_lazy
 from .managers import UserManager
 
 
@@ -23,9 +23,18 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
+    def get_absolute_url(self):
+        return reverse_lazy('profiles:profile-detail', args=[self.id])
+
     def full_name(self):
         return super().get_full_name()
 
     def email_verified(self):
         return self.emailaddress_set.get(primary=True).verified
     email_verified.boolean = True
+
+    def followers_count(self) -> int:
+        return self.followers.count()
+
+    def following_count(self) -> int:
+        return self.following.count()

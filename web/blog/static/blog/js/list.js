c40831913a24a1b9d7831ada877e8381c8850300
Artur <art05xxx@rambler.ru>
2021-08-26 07:09:07 +0300

    241 created scroll function

--- web/blog/static/blog/js/list.js
+++ web/blog/static/blog/js/list.js
@@ -4,3 +4,24 @@ $(function () {
 });
 
 console.log('blog-list')
+
+
+
+$(window).scroll(function () {
+// End of the document reached?
+    let pagination = $('#pagination')
+
+    console.log(pagination.height() - $(this).height() , $(this).scrollTop())
+    if (pagination.height() - $(this).height() <= $(this).scrollTop()) {
+        $.ajax({
+            type: "GET",
+            url: pagination.attr('data-href'),
+            success: function (data) {
+              console.log(data)
+            },
+            error: function (req, status, error) {
+
+            }
+        });
+    }
+});

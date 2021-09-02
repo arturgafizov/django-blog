bc25b8b0bfa1f091c77cf59eaaa6aabb1ec99d52
Artur <art05xxx@rambler.ru>
2021-09-02 06:46:45 +0300

    260 delete scroll function

--- web/profiles/static/profile/follower.js
+++ web/profiles/static/profile/follower.js
@@ -37,30 +37,6 @@ function follow(event) {
       })
 }
 
-$(window).scroll(function () {
-    // End of the document reached?
-    let url = window.location.pathname
-    console.log(url)
-    if ($(document).height() - $(this).height() == $(this).scrollTop()) {
-
-        $.ajax({
-            type: "GET",
-            url: "/profiles/profile/2",
-            contentType: "application/json; charset=utf-8",
-            data: ' ',
-            dataType: "json",
-            success: function (msg) {
-                if (msg.d) {
-                    $(".row").append(msg.d);
-
-                }
-            },
-            error: function (req, status, error) {
-                  alert("Error try again");
-            }
-        });
-    }
-});
 
 function modalRender(data) {
     let body = $('#followModalBody')

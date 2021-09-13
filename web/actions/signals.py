c7b233f31c6568897f2944c28691116311bbcb8e
Artur <art05xxx@rambler.ru>
2021-09-13 07:04:51 +0300

    276 created user_started_follow, user_finished_follow, user_changed_avatar

--- web/actions/signals.py
+++ web/actions/signals.py
@@ -1,7 +1,10 @@
 from django.contrib.auth import get_user_model
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from django.template.loader import render_to_string
 from rest_framework.reverse import reverse_lazy
+
+from profiles.models import Profile
 from .models import Follower, LikeDislike, UserAction
 from .services import ActionsService
 from .choices import UserActionChoices
@@ -10,20 +13,41 @@ User = get_user_model()
 
 
 @receiver(post_save, sender=Follower)
-def user_started_follow(sender, created: bool, instance: Follower, **kwargs):
+def user_started_follow(sender, instance: Follower, **kwargs):
+    print(sender, instance)
+    template = 'actions/start_follow.html'
+    data = {
+        'subscriber': instance.subscriber,
+        'to_user': instance.to_user,
+    }
+
+    action = render_to_string(template, data)
+    user = instance.subscriber
+    ActionsService.create_action(user, action, instance)
+
+
+@receiver(post_save, sender=Follower)
+def user_finished_follow(sender, instance: Follower, **kwargs):
+    print(sender, instance)
+    template = 'actions/finish_follow.html'
+    data = {
+        'subscriber': instance.subscriber,
+        'to_user': instance.to_user,
+    }
+
+    action = render_to_string(template, data)
+    user = instance.subscriber
+    ActionsService.create_action(user, action, instance)
+
+
+@receiver(post_save, sender=Profile)
+def user_changed_avatar(sender, created: bool, instance: Profile, **kwargs):
     print(sender, created, instance)
+    template = 'actions/change_avatar.html'
+    data = {
+        'avatar': instance.avatar,
+    }
 
-    if created:
-        user = instance.subscriber
-
-        subscriber = "<a href='{url}'> {full_name} </a>".format(
-            url=reverse_lazy('profiles:profile-detail', kwargs={'pk': user.id}),
-            full_name=user.full_name()
-        )
-        to_user = instance.to_user
-        to_user = "<a href='{url}'> {full_name} </a>".format(
-            url=reverse_lazy('profiles:profile-detail', kwargs={'pk': to_user.id}),
-            full_name=to_user.full_name()
-        )
-        action: str = UserActionChoices.FOLLOW_TO.label.format(subscriber=subscriber, to_user=to_user)
-        ActionsService.create_action(user, action, instance)
+    action = render_to_string(template, data)
+    user = instance.user
+    ActionsService.create_action(user, action, instance)

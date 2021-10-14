fe70b4198bfaf66824b3d541d84c0aba5b98e14b
Artur <art05xxx@rambler.ru>
2021-10-14 07:33:56 +0300

    292 add url user_jwt

--- web/main/urls.py
+++ web/main/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 from django.views.generic import RedirectView
-from .views import UserView, IndexView
+from .views import UserView, IndexView, UserJwtView
 from django.conf import settings
 
 
 urlpatterns = [
     path('user/', UserView.as_view()),
+    path('user/jwt/', UserJwtView.as_view(), name='user_jwt'),
 ]
 
 if settings.ENABLE_RENDERING:

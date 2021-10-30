f1aa08460cadfe60e3d7733f531f051e1ddfd728
Artur <art05xxx@rambler.ru>
2021-10-30 20:32:15 +0300

    294 add user_id

--- web/profiles/static/profile/chat.js
+++ web/profiles/static/profile/chat.js
@@ -11,7 +11,10 @@ function chatInit() {
     let button = $(this);
     let jwt = localStorage.getItem('jwt')
     let url = button.data('href')
-    url = url + '?jwt='+jwt
+    let user_id = button.data('userid')
+  console.log(button)
+    console.log(user_id)
+    url = url + '?jwt='+jwt + '&user_id=' + user_id
     window.open(url, '_blank').focus()
 
 }
